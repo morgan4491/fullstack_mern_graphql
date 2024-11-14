d00868953f9e71c2914e4f890f24d9d429237346
morgan4491 <m.morgan4491@gmail.com>
2024-11-14 14:54:23 -0500

    set up client side graphql and started formatting pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.tsx';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
 )
