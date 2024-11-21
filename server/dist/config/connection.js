62bb624d430ad20bca8223ad837e31076366ab38
morgan4491 <m.morgan4491@gmail.com>
2024-11-21 13:49:13 -0500

    fixed deploy code in server and separated authenticate into a file

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,3 +1,5 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/pet_social_network_db'); // The end part is the database name. If it does not exist, it will be created
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/pet_social_network_db'); // The end part is the database name. If it does not exist, it will be created
 export default mongoose.connection;
--- server/dist/server.js
+++ server/dist/server.js
@@ -2,12 +2,11 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cookieParser from 'cookie-parser';
-import jwt from 'jsonwebtoken';
+import path from 'path';
 import connection from './config/connection.js';
+import { authenticate } from './services/auth.js';
 import typeDefs from './schema/typeDefs.js';
 import resolvers from './schema/resolvers.js';
-import User from './models/User.js';
-const { verify } = jwt;
 const app = express();
 const PORT = process.env.PORT || 3333;
 const server = new ApolloServer({
@@ -20,38 +19,18 @@ connection.once('open', async () => {
     app.use('/graphql', express.json(), 
     // Allow the resolvers to access client-side cookies through context.req.cookies
     cookieParser(), expressMiddleware(server, {
-        // Attach the context object for all resolvers - The return value of this function is what your context will be
-        context: async ({ req, res }) => {
-            const pet_token = req.cookies?.pet_token;
-            if (pet_token) {
-                try {
-                    if (!process.env.JWT_SECRET) {
-                        console.log('MUST ADD JWT_SECRET TO .env!');
-                        return {
-                            req: req,
-                            res: res
-                        };
-                    }
-                    const userData = verify(pet_token, process.env.JWT_SECRET);
-                    if (!userData || typeof userData === 'string') {
-                        return {
-                            req: req,
-                            res: res
-                        };
-                    }
-                    const user = await User.findById(userData.user_id);
-                    req.user = user;
-                }
-                catch (error) {
-                    console.log('JWT VERIFICATION ERROR', error);
-                }
-            }
-            return {
-                req: req,
-                res: res
-            };
-        }
+        // Attach the context object for all resolvers by referencing a function that returns an object with req and res, and if they have a valid cookie/jwt, req.user will be their user object
+        context: authenticate
     }));
+    if (process.env.PORT) {
+        const __dirname = path.dirname(new URL(import.meta.url).pathname);
+        //Share all files in the client/dist folder with the client-side
+        app.use(express.static(path.join(__dirname, '../../client/dist')));
+        // Create a wildcard route that sends the client/dist/index.htm. file back to the client-side
+        app.get('*', (_, res) => {
+            res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+        });
+    }
     app.listen(PORT, () => {
         console.log('Express server started on', PORT);
     });
